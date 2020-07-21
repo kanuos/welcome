f099219093e9a6274ce474c5a2e2c41c3535b3e6
kanuos <sounakmukherjee@ymail.com>
2020-07-21 16:03:30 -0400

    converter component functionality

---
+++ src/conversion.js
@@ -0,0 +1,43 @@
+export default {
+    mass : {
+        grams : "grams",
+        kilograms : "kilograms",
+        pounds : "poundsUSUK",
+        stones : "stones",
+        tonnes : "tonnesMetric",
+        ounces : "ouncesUSUK"
+    },
+    length : {
+        feet : "feetUKUS",
+        inches : "inches",
+        centimeters : "centimeters",
+        kilometers : "kilometers",
+        meters :  "meters",
+        furlongs : "furlongs",
+        yards : "yards",
+        miles : "milesUKUS"
+    },
+    area : {
+        acres : "acres",
+        hectares : "hectares",
+        squareKilometers : "squareKilometers",
+        squareMeters : "squareMeters",
+        squareCentimeters : "squareCentimeters",
+        squareMiles : "squareMiles",
+        squareYards : "squareYards",
+        squareFeet : "squareFeetUSUK"
+    },
+    temperature : {
+        celsius : "celsius",
+        kelvin : "kelvin",
+        fahrenheit : "fahrenheit",
+        rankine : "rankine",
+        reaumur : "reaumur"
+    },
+    time : {
+        minutes : "minutes",
+        seconds : "seconds",
+        milliseconds: "milliseconds",
+        hours : "hours"
+    }
+}
--- src/views/Converter.vue
+++ src/views/Converter.vue
@@ -4,33 +4,75 @@
     <div>
         <section>
             <article class="converter">
-                <h1 v-if="tabText.length > 0">
+                <h2 v-if="tabText.length > 0">
                     {{tabText}} Converter
-                </h1>
+                </h2>
+                <form @submit.prevent="convertUnits">
+                    <input 
+                        type="text" 
+                        v-model="inputValue"
+                        :placeholder="tabText">
+                    <select v-model="input">
+                        <option 
+                            v-for="(opt,index) in Object.keys(units[tabText.toLowerCase()])"
+                            :key="index"
+                            :value="opt"
+                            >
+                            {{opt}}
+                        </option>
+                    </select>
+                    <select v-model="output">
+                        <option 
+                            v-for="(opt,index) in Object.keys(units[tabText.toLowerCase()])"
+                            :key="index"
+                            :value="opt"
+                            >
+                            {{opt}}
+                        </option>
+                    </select>
+                    <button type="submit">Convert</button>
+                </form>
+                <h5 v-if="errorMessage">
+                    {{errorMessage}}
+                </h5>
+                <img src="../assets/loader.gif" v-if="isLoading">
+                <h3 v-else>
+                    {{outputValue}}
+                </h3>
             </article>
         </section>
         <aside>
             <button :class="tab == 1 && 'active'" @click="tabSelector(1)">Length</button>
             <button :class="tab== 2 && 'active'" @click="tabSelector(2)">Area</button>
-            <button :class="tab== 3 && 'active'" @click="tabSelector(3)">Volume</button>
-            <button :class="tab== 4 && 'active'" @click="tabSelector(4)">Mass</button>
-            <button :class="tab== 5 && 'active'" @click="tabSelector(5)">Speed</button>
-            <button :class="tab== 6 && 'active'" @click="tabSelector(6)">Temperature</button>
-            <button :class="tab== 7 && 'active'" @click="tabSelector(7)">Time</button>
-            <button :class="tab== 8 && 'active'" @click="tabSelector(8)">Currency</button>
+            <button :class="tab== 3 && 'active'" @click="tabSelector(3)">Mass</button>
+            <button :class="tab== 4 && 'active'" @click="tabSelector(4)">Temperature</button>
+            <button :class="tab== 5 && 'active'" @click="tabSelector(5)">Time</button>
+            <button :class="tab== 6 && 'active'" @click="tabSelector(6)">Currency</button>
         </aside>
     </div>
 </main>
 </template>
 
 <script>
-import Navbar from '../components/Navbar.vue'
+import Navbar from '../components/Navbar.vue';
+import units from '../conversion';
+import Converter from 'unit-converter-pro';
+import axios from 'axios';
+
 export default {
     components : {Navbar},
     data(){
         return {
             tab : 1,
             tabText : "Length",
+            units,
+            input : "",
+            output : "",
+            inputValue : "",
+            outputValue: "",
+            currency : {},
+            errorMessage : "",
+            isLoading : false
         }
     },
     methods : {
@@ -39,22 +81,113 @@ export default {
             switch(tab){
                 case 1 : this.tabText = "Length"; break;
                 case 2 : this.tabText = "Area"; break;
-                case 3 : this.tabText = "Volume"; break;
-                case 4 : this.tabText = "Mass/Weight"; break;
-                case 5 : this.tabText = "Speed"; break;
-                case 6 : this.tabText = "Temperature"; break;
-                case 7 : this.tabText = "Time"; break;
-                case 8 : this.tabText = "Currency"; break;
+                case 3 : this.tabText = "Mass"; break;
+                case 4 : this.tabText = "Temperature"; break;
+                case 5 : this.tabText = "Time"; break;
+                case 6 : this.tabText = "Currency"; break;
                 default: this.tabText = "Length"; break;
             }
+        },
+    convertUnits(){
+        if (this.inputValue && this.input.length && this.output.length){
+            this.isLoading = true;        
+            const key = this.tabText.toLowerCase();
+            const from = this.units[key][this.input];
+            const to = this.units[key][this.output];
+            const value = parseFloat(this.inputValue.trim());
+
+            try {
+                let obj = {};
+                switch(this.tabText){
+                    case "Length": 
+                        obj = new Converter.Length();
+                        this.outputValue = Math.round(obj[from](value)[to]());
+                        break;
+                    case "Area":
+                        obj= new Converter.Area();
+                        this.outputValue = Math.round(obj[from](value)[to]());
+                         break;
+                    case "Mass":
+                        obj= new Converter.Mass()
+                        this.outputValue = Math.round(obj[from](value)[to]());
+                        break;
+                    case "Temperature":
+                        obj= new Converter.Temperature()
+                        this.outputValue = Math.round(obj[from](value)[to]());
+                        break;
+                    case "Time":
+                        obj= new Converter.Temperature()
+                        this.outputValue = Math.round(obj[from](value)[to]());
+                        break;
+                    case "Currency":
+                        this.outputValue = this.currencyConverter()
+                        break;
+                }
+                setTimeout(()=> {
+                    this.isLoading = false;
+                }, 1200)
+            }
+            catch(e){
+                this.errorMessage = "Something went wrong. Try again later."
+                }
+            }
+            else {
+                this.errorMessage = "Cannot convert empty values"
+            }
+        },
+        currencyConverter(){
+            const value = parseFloat(this.inputValue);
+            return value * this.currency[this.output] / this.currency[this.input]
+        },
+        async requestCurrencyChart(){
+            this.isLoading = true;
+            const URL = `https://api.exchangeratesapi.io/latest`
+            const response = await axios(URL);
+            const {date, base, rates} = response.data;
+            rates[base] = 1;
+            sessionStorage.setItem('welcome-data',JSON.stringify({rates,date}));
+            this.currency = rates;
+            this.isLoading = false;
+            this.units.currency =  this.currency;
+        }
+    },
+    created() {
+        const currencyRates = JSON.parse(sessionStorage.getItem('welcome-data'));
+        
+        if (currencyRates){
+            const {date} = currencyRates;
+            
+            if (new Date(Date.now()).toDateString() !== new Date(date).toDateString()) {
+                this.requestCurrencyChart();
+            }
+            else {
+                this.currency = currencyRates.rates;
+            }
+        }
+        else{
+            this.requestCurrencyChart();
+        }
+    },
+    updated(){
+        if (this.errorMessage.length > 0){
+            setTimeout(()=> {
+                this.errorMessage = "";
+            },1500)
         }
     }
 }
 </script>
 
 <style scoped>
-    li {
+    /* li {
         color: white;
+    } */
+    h2 {
+        color: black;
+        text-transform: uppercase;
+        font-size: 2rem;
+        border-bottom: 1px solid black;
+        padding-bottom: 1.5rem;
     }
     div {
         display: grid;
@@ -100,10 +233,12 @@ export default {
         margin: auto;
         border-radius: .25rem;
         box-shadow: inset 0 1px 1rem black;
-        display: grid;
         padding: 1.5rem;
     }
-
+    form {
+        height: 100%;
+        background-color: red;
+    }
 
     @media (min-width: 750px){
         div {
